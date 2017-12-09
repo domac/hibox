8a5c6e135e3b8a1f500115a2c099ceedb5bd7276
domac.li <domacode@gmail.com>
2017-12-09 20:05:38 +0800

    1st

--- basic/teamtest/diff_count/main.go
+++ basic/teamtest/diff_count/main.go
@@ -1,5 +1,55 @@
 package main
 
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"time"
+)
+
+var splitB = []byte(":")
+
+var statsMap = make(map[string]int64, 4096)
+
+func readAndHandleDataFile(filepath string) {
+	f, err := os.Open(filepath)
+	if err != nil {
+		return
+	}
+	defer func() {
+		f.Close()
+	}()
+	s := bufio.NewScanner(f)
+
+	for s.Scan() {
+		if b := s.Bytes(); b != nil {
+			//readCh <- bytes.Split(b, splitB)
+			bs := bytes.Split(b, splitB)
+			v, _ := strconv.ParseInt(string(bs[1]), 10, 0)
+			statsMap[string(bs[0])] += v
+		}
+	}
+	log.Printf("read  %s completed !\n", filepath)
+}
+
 func main() {
-	println("hello")
+	args := os.Args
+	if len(args) < 2 {
+		panic("please tell me the data file")
+	}
+	dataFile := args[1]
+
+	start := time.Now()
+	if dataFile != "" {
+		readAndHandleDataFile(dataFile)
+	}
+	elapsed := time.Now().Sub(start)
+	log.Printf("total elapsed time: %f seconds", elapsed.Seconds())
+
+	for k, v := range statsMap {
+		fmt.Printf("[%s]:%d\n", k, v)
+	}
 }
