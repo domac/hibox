92a4286ab46a7d5f8ac5ef6edee274b1959ec825
domac.li <domacode@gmail.com>
2017-12-10 12:27:58 +0800

    改进整型解析的方法

--- basic/teamtest/diff_count/main.go
+++ basic/teamtest/diff_count/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bufio"
 	"bytes"
-	"errors"
 	"fmt"
 	"log"
 	"os"
@@ -11,33 +10,24 @@ import (
 	"unsafe"
 )
 
-var splitB = []byte(":")
-
 var statsMap = make(map[string]uint64, 4096)
-
-const intSize = 32 << (^uint(0) >> 63)
-const maxUint64 = (1<<64 - 1)
-
-var ErrRange = errors.New("value out of range")
-var ErrSyntax = errors.New("invalid syntax")
 
 func bytesToString(b []byte) string {
 	return *(*string)(unsafe.Pointer(&b))
 }
 
-func parseUint(s string) (uint64, error) {
+func parsebyteToUint(b []byte) uint64 {
 	var n uint64
-	var err error
 	i := 0
-	for ; i < len(s); i++ {
+	for ; i < len(b); i++ {
 		var v byte
-		d := s[i]
+		d := b[i]
 		v = d - '0'
 		n *= uint64(10)
 		n1 := n + uint64(v)
 		n = n1
 	}
-	return n, err
+	return n
 }
 
 func readAndHandleDataFile(filepath string) {
@@ -53,10 +43,7 @@ func readAndHandleDataFile(filepath string) {
 	for s.Scan() {
 		if b := s.Bytes(); b != nil {
 			idx := bytes.IndexByte(b, ':')
-			k := b[0:idx]
-			v := b[idx+1:]
-			count, _ := parseUint(bytesToString(v))
-			statsMap[string(k)] += count
+			statsMap[string(b[0:idx])] += parsebyteToUint(b[idx+1:])
 		}
 	}
 	log.Printf("read  %s completed !\n", filepath)
