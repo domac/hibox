f4f199a70dccb99c2b9c90abc1e684b7afd5a9e0
domac.li <domacode@gmail.com>
2017-12-10 14:54:18 +0800

    优化哈希

--- basic/teamtest/diff_count/main.go
+++ basic/teamtest/diff_count/main.go
@@ -7,32 +7,25 @@ import (
 	"log"
 	"os"
 	"time"
-	"unsafe"
 )
 
-var statsMap = make(map[uint64]uint64, 4096)
-var nameMap = make(map[uint64]string, 256)
+var statsMap = make(map[uint64]uint64, 1024)
+var nameMap = make(map[uint64]string, 1024)
 
-func bytes64(data []byte) uint64 {
-	var v uint64 = 14695981039346656037
-	for _, c := range data {
-		v = (v ^ uint64(c)) * 1099511628211
-	}
-	return v
-}
-
+//自定义哈希函数
 func hashFunc(data []byte) uint64 {
-	h := bytes64(data)
+	var h uint64 = 14695981039346656037
+	for _, c := range data {
+		h = (h ^ uint64(c)) * 20171111
+	}
+
 	if _, ok := nameMap[h]; !ok {
 		nameMap[h] = string(data)
 	}
 	return h
 }
 
-func bytesToString(b []byte) string {
-	return *(*string)(unsafe.Pointer(&b))
-}
-
+//把字符数组转化为无符号整型
 func parsebyteToUint(b []byte) uint64 {
 	var n uint64
 	i := 0
@@ -47,6 +40,7 @@ func parsebyteToUint(b []byte) uint64 {
 	return n
 }
 
+//处理数据文件
 func readAndHandleDataFile(filepath string) {
 	f, err := os.Open(filepath)
 	if err != nil {
@@ -56,15 +50,14 @@ func readAndHandleDataFile(filepath string) {
 		f.Close()
 	}()
 	s := bufio.NewScanner(f)
-
 	for s.Scan() {
 		if b := s.Bytes(); b != nil {
-			idx := bytes.IndexByte(b, ':')
-			hashVal := hashFunc(b[0:idx])
+			idx := bytes.IndexByte(b, ':') //分隔符所在索引位置
+			hashVal := hashFunc(b[0:idx])  //计算哈希值
 			statsMap[hashVal] += parsebyteToUint(b[idx+1:])
 		}
 	}
-	log.Printf("read  %s completed !\n", filepath)
+	log.Printf("read %s completed !\n", filepath)
 }
 
 func main() {
