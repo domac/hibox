5942c3ebc245b53826265338a0f430bb23ab75e5
domac.li <domacode@gmail.com>
2017-12-20 00:08:51 +0800

    add

--- basic/teamtest/brand_count/brand/brand.go
+++ basic/teamtest/brand_count/brand/brand.go
@@ -2,7 +2,6 @@ package brand
 
 import (
 	"bufio"
-	"bytes"
 	"fmt"
 	"os"
 )
@@ -12,22 +11,12 @@ const TOPNUM = 40
 
 //全局变量
 var (
-	BRANDKEYS       = make(map[uint64]int, 1024*1024*20)
-	BRANDDB         = make(map[uint64]uint64, 1024*1024)
-	TARGET_STORE    = []byte("VIP_NH")
-	CurrentMinValue uint64 //当前最小值
-	toplist         [TOPNUM]BrandItem
-	topMap          = make(map[uint64]int)
+	BRANDKEYS = make(map[uint64]int, 30000000)
+	//BRANDDB      = make(map[uint64]int, 1024*1024)
+	BRANDDB = []int{}
+	toplist [TOPNUM]uint64
+	topMap  = make(map[uint64]int)
 )
-
-//品牌项
-type BrandItem struct {
-	Name       string
-	TotalValue uint64
-	HashKey    uint64
-
-	seq int
-}
 
 func InitKeys(dataFile string) error {
 	f, err := os.Open(dataFile)
@@ -38,63 +27,19 @@ func InitKeys(dataFile string) error {
 	s := bufio.NewScanner(f)
 	idx := 0
 	for s.Scan() {
-		if b := s.Bytes(); b != nil {
-			//逆向切割
-			hashKey := hashBytes(b)
-			BRANDKEYS[hashKey] = idx
-			idx++
-		}
+		b := s.Bytes()
+		//逆向切割
+		hashKey := hashBytes(b)
+		BRANDKEYS[hashKey] = idx
+		idx++
 	}
+	keysLen := idx
+	BRANDDB = make([]int, keysLen, keysLen)
+	for i := 0; i < keysLen; i++ {
+		BRANDDB[i] = 0
+	}
+	println(len(BRANDDB))
 	return nil
-}
-
-//获取数组的最小项
-func getArrayMinValue(filterkey uint64) (BrandItem, int) {
-	minItem := toplist[0]
-	minidx := 0
-	ilen := len(toplist)
-	for i := 1; i < ilen; i++ {
-		if toplist[i].TotalValue < minItem.TotalValue {
-			minItem = toplist[i]
-			minidx = i
-		} else if toplist[i].TotalValue == minItem.TotalValue {
-			if BRANDKEYS[toplist[i].HashKey] > BRANDKEYS[minItem.HashKey] {
-				minItem = toplist[i]
-				minidx = i
-			}
-		}
-	}
-	return minItem, minidx
-}
-
-func sendToTopList(name []byte, hashKey, brandValue uint64) {
-	idx, ok := topMap[hashKey]
-	if !ok {
-		minItem, mIndex := getArrayMinValue(hashKey)
-
-		isRelpace := false
-
-		if brandValue > minItem.TotalValue {
-			isRelpace = true
-		} else if brandValue == minItem.TotalValue {
-			if BRANDKEYS[hashKey] < BRANDKEYS[minItem.HashKey] {
-				isRelpace = true
-			}
-		}
-		//取代之前最小值
-		if isRelpace {
-			tempKey := minItem.HashKey
-			minItem.HashKey = hashKey
-			minItem.TotalValue = brandValue
-			minItem.Name = string(name)
-			delete(topMap, tempKey)
-			topMap[hashKey] = mIndex
-			toplist[mIndex] = minItem
-		}
-	} else {
-		toplist[idx].TotalValue = brandValue
-	}
-
 }
 
 //数据文件读入处理
@@ -107,66 +52,58 @@ func ReadAndHandle(dataFile string) error {
 	defer f.Close()
 	s := bufio.NewScanner(f)
 	for s.Scan() {
-		if b := s.Bytes(); b != nil {
-			//逆向切割
-			bs := genSpaceSplit(b)
+		b := s.Bytes()
+		lenB := len(b)
+		index1 := lasIndexN(b, 9, 32)
+		//onlineDate := b[index1+1:]
 
-			{ //限定上架年份和仓库
-				age := parsebyteToUint64(bs[0][2:4])
-				name := bs[4]
-				if bytes.Compare(bs[2], TARGET_STORE) == 0 && (age >= 11 && age < 17) {
-					hashKey := hashBytes(name)
-					current_brand_value := BRANDDB[hashKey] + parsebyteToUint64(bs[1])
-					BRANDDB[hashKey] = current_brand_value
-					sendToTopList(name, hashKey, current_brand_value)
-				}
-			}
+		if b[index1+3] != 49 {
+			continue
 		}
+
+		age := b[index1+4]
+		if age != 54 && age != 53 && age != 52 && age != 51 && age != 50 {
+			continue
+		}
+
+		len1 := (lenB - index1 - 1)
+		index2 := lasIndexN(b, len1+2, 32)
+		price := b[index2+1 : index1]
+
+		len2 := index1 - index2 - 1
+		len21 := len1 + len2
+		index3 := lasIndexN(b, len21+6, 32)
+		//store := b[index3+1 : index2]
+
+		//not equals 'N'
+		if b[index3+5] != 78 {
+			continue
+		}
+
+		len3 := index2 - index3 - 1
+		index4 := lasIndexN(b, len21+len3+5, 32)
+		name := b[:index4]
+
+		hashKey := hashBytes(name)
+		if xh, ok := BRANDKEYS[hashKey]; ok {
+			BRANDDB[xh] += parsebyteToInt(price)
+			updateTopList(name, hashKey, xh)
+		}
+
+		// onlineDate := b[index1+1:]
+		// fmt.Printf("%s\n", onlineDate)
+		// fmt.Printf("%s\n", price)
+		// fmt.Printf("%s\n", name)
+		// println("-----------")
 	}
 	return nil
 }
 
-//输出结果
-func ListResult() {
-	values := make([]BrandItem, TOPNUM)
-	for i, item := range toplist {
-		values[i] = item
-	}
-	quickSort(values, 0, len(values)-1)
-	for i, item := range values {
-		if item.HashKey == 0 {
-			continue
-		}
-		fmt.Printf("(%d) name: %s | value: %d\n", (i + 1), item.Name, item.TotalValue)
-	}
-	topMap = nil
-	BRANDDB = nil
-	fmt.Println("------- finish -------")
+func updateTopList(name []byte, hashKey uint64, xh int) {
+
 }
 
-//快速排序:从大到小
-func quickSort(arr []BrandItem, start, end int) {
-	if start < end {
-		i, j := start, end
-		key := arr[(start+end)/2].TotalValue
-		for i <= j {
-			for arr[i].TotalValue > key {
-				i++
-			}
-			for arr[j].TotalValue < key {
-				j--
-			}
-			if i <= j {
-				arr[i], arr[j] = arr[j], arr[i]
-				i++
-				j--
-			}
-		}
-		if start < j {
-			quickSort(arr, start, j)
-		}
-		if end > i {
-			quickSort(arr, i, end)
-		}
-	}
+//输出结果
+func ListResult() {
+	fmt.Println("------- finish -------")
 }
--- basic/teamtest/brand_count/brand/util.go
+++ basic/teamtest/brand_count/brand/util.go
@@ -34,6 +34,24 @@ func lasIndex(s []byte, c byte) int {
 	return -1
 }
 
+func lasIndexN(s []byte, n int, c byte) int {
+	for i := len(s) - n; i >= 1; i-- {
+		if s[i] == c {
+			return i
+		}
+	}
+	return -1
+}
+
+func lasIndexSE(s []byte, n int, begin int, c byte) int {
+	for i := len(s) - n; i >= begin; i-- {
+		if s[i] == c {
+			return i
+		}
+	}
+	return -1
+}
+
 //自定义哈希函数
 func hashBytes(data []byte) uint64 {
 	var h uint64 = 14695981039346656037
@@ -48,9 +66,21 @@ func parsebyteToUint64(b []byte) (n uint64) {
 	for i := 0; i < len(b); i++ {
 		var v byte
 		d := b[i]
-		v = d - '0'
+		v = d - 48
 		n *= uint64(10)
 		n1 := n + uint64(v)
+		n = n1
+	}
+	return n
+}
+
+func parsebyteToInt(b []byte) (n int) {
+	lenb := len(b)
+	for i := 0; i < lenb; i++ {
+		d := b[i]
+		v := d - 48
+		n *= 10
+		n1 := n + int(v)
 		n = n1
 	}
 	return n
