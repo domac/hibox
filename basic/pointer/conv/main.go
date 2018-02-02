4fdf3f44dfc582c97e3e777b083f180b8e2d769a
domac.li <domacode@gmail.com>
2018-02-02 14:17:46 +0800

    add

---
+++ basic/pointer/conv/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"unsafe"
+)
+
+type entryHdr struct {
+	accessTime uint32
+	keyLen     uint16
+	deleted    bool
+	testId     uint8
+}
+
+const ENTRY_HDR_SIZE = 8
+
+func main() {
+	var hdrBuf [ENTRY_HDR_SIZE]byte
+	hdr := (*entryHdr)(unsafe.Pointer(&hdrBuf[0]))
+
+	fmt.Printf("------ > %v\n", hdr)
+	now := uint32(time.Now().Unix())
+	hdr.accessTime = now
+	hdr.testId = 12
+	hdr.keyLen = 20
+	hdr.deleted = true
+	fmt.Printf("------ > %v\n", hdr)
+}
