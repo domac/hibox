79f77fae070713989367270c2b8121bd6c08f65b
domac <domacode@gmail.com>
2018-09-10 22:04:19 +0800

    add

--- basic/redispubsub/redispubsub.go
+++ basic/redispubsub/redispubsub.go
@@ -131,6 +131,63 @@ func (r *RedisNotify) GetSubChannel(subKey string, channelsize int) (<-chan []by
 	return mq, nil
 }
 
+func (r *RedisNotify) SubscribeHooks(hooks map[string]func(string)) error {
+
+	hooksLen := len(hooks)
+
+	if hooksLen == 0 {
+		return errors.New("hooks length must more than zero")
+	}
+
+	client, err := r.redisOpenHook()
+	if err != nil {
+		return err
+	}
+	//测试连通性
+	_, err = client.Ping().Result()
+	if err != nil {
+		return err
+	}
+
+	subKeys := make([]string, hooksLen)
+	for k, _ := range hooks {
+		subKeys = append(subKeys, k)
+	}
+
+	log.Printf("subkeys => %v\n", subKeys)
+
+	pubsub := client.Subscribe(subKeys...)
+	defer pubsub.Close()
+
+	for {
+		select {
+		case <-r.stop:
+			log.Println("redis notify is stop")
+			return errors.New("redis notify is stop")
+
+		default:
+			msg, err := pubsub.ReceiveMessage()
+			if err != nil {
+				log.Println(err.Error())
+				if err.Error() == "redis: client is closed" {
+					panic(err)
+				} else {
+					continue
+				}
+			}
+
+			if hookfunc, ok := hooks[msg.Channel]; ok {
+				log.Printf("receive a hook notify : %s\n", msg.Channel)
+				if hookfunc != nil {
+					hookfunc(msg.Payload)
+				}
+			} else {
+				log.Printf("receive a bad hook notify : %s\n", msg.Channel)
+			}
+		}
+	}
+}
+
 //设置关闭回调
 func (r *RedisNotify) SetRedisCloseHook(fn CloseRedisClientFunc) {
 	r.redisCloseHook = fn
