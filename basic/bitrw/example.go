808ec76430ede021376f55278d246816b06329f7
domac <domacode@gmail.com>
2018-10-02 21:36:49 +0800

    add

--- basic/bitrw/example.go
+++ basic/bitrw/example.go
@@ -3,7 +3,7 @@ package main
 import (
 	"encoding/binary"
 	"fmt"
-	"io"
+	//"io"
 )
 
 type ByteBuffer struct {
@@ -19,36 +19,37 @@ func NewByteBuffer() *ByteBuffer {
 func (b *ByteBuffer) Len() int {
 	return len(b.B)
 }
-func (b *ByteBuffer) ReadFrom(r io.Reader) (int64, error) {
-	p := b.B
-	nStart := int64(len(p))
-	nMax := int64(cap(p))
-	n := nStart
-	if nMax == 0 {
-		nMax = 64
-		p = make([]byte, nMax)
-	} else {
-		p = p[:nMax]
-	}
-	for {
-		if n == nMax {
-			nMax *= 2
-			bNew := make([]byte, nMax)
-			copy(bNew, p)
-			p = bNew
-		}
-		nn, err := r.Read(p[n:])
-		n += int64(nn)
-		if err != nil {
-			b.B = p[:n]
-			n -= nStart
-			if err == io.EOF {
-				return n, nil
-			}
-			return n, err
-		}
-	}
-}
+
+// func (b *ByteBuffer) ReadFrom(r io.Reader) (int64, error) {
+// 	p := b.B
+// 	nStart := int64(len(p))
+// 	nMax := int64(cap(p))
+// 	n := nStart
+// 	if nMax == 0 {
+// 		nMax = 64
+// 		p = make([]byte, nMax)
+// 	} else {
+// 		p = p[:nMax]
+// 	}
+// 	for {
+// 		if n == nMax {
+// 			nMax *= 2
+// 			bNew := make([]byte, nMax)
+// 			copy(bNew, p)
+// 			p = bNew
+// 		}
+// 		nn, err := r.Read(p[n:])
+// 		n += int64(nn)
+// 		if err != nil {
+// 			b.B = p[:n]
+// 			n -= nStart
+// 			if err == io.EOF {
+// 				return n, nil
+// 			}
+// 			return n, err
+// 		}
+// 	}
+// }
 
 func (b *ByteBuffer) Bytes() []byte {
 	return b.B
@@ -79,19 +80,19 @@ func (b *ByteBuffer) Read(p []byte) (int, error) {
 func main() {
 	bb := NewByteBuffer()
 	binary.Write(bb, binary.BigEndian, uint32(0))
-	fmt.Printf("[debug]: %v\n", bb)
+	fmt.Printf("[debug]: %v\n", bb.B)
 
 	bb.WriteByte('1')
 
-	fmt.Printf("[debug]: %v\n", bb)
+	fmt.Printf("[debug]: %v\n", bb.B)
 
 	bb.WriteByte(byte(128))
 
-	fmt.Printf("[debug]: %v\n", bb)
+	fmt.Printf("[debug]: %v\n", bb.B)
 
 	bb.Write([]byte{'1', '2', '3', '4'})
 
-	fmt.Printf("-[debug]: %v\n", bb)
+	fmt.Printf("-[debug]: %v\n", bb.B)
 
 	prefixLen := bb.Len()
 
@@ -100,18 +101,33 @@ func main() {
 	//更新size
 	binary.BigEndian.PutUint32(bb.B, uint32(bb.Len()))
 
-	fmt.Printf("--[debug]: %v\n", bb)
+	fmt.Printf("--[debug]: %v\n", bb.B)
 
 	println("---------------------------")
 	binary.Write(bb, binary.BigEndian, uint64(64))
 	fmt.Printf("[debug]: %v\n", bb)
 	bb.WriteByte('p')
-	fmt.Printf("[debug]: %v\n", bb)
+	fmt.Printf("[debug]: %v\n", bb.B)
 
 	println("---------------------------")
 	var size uint32
 	binary.Read(bb, binary.BigEndian, &size)
 	fmt.Printf("[debug]: size=%v\n", size)
-	fmt.Printf("[debug]: data=%v\n", bb)
+	fmt.Printf("[debug]: data=%v\n", bb.B)
+
+	println("---------------------------")
+
+	prefixLen = bb.Len()
+
+	println("current prefixLen =", prefixLen)
+
+	//更新size
+	binary.BigEndian.PutUint32(bb.B, uint32(bb.Len()))
+
+	fmt.Printf("[debug]: %v\n", bb.B)
+
+	binary.BigEndian.PutUint16(bb.B[4:], uint16(8))
+
+	fmt.Printf("[debug]: %v\n", bb.B)
 
 }
